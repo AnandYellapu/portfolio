5a0b737d1d8b57921377427a953883e1aa56b906
AnandYellapu <anandsaiii1200@gmail.com>
2023-10-07 18:57:07 +0530

    update

--- src/components/Homepage/Homepage.js
+++ src/components/Homepage/Homepage.js
@@ -59,29 +59,92 @@
 
 
 
+// import React from 'react';
+// import '../../pages/style.css';
+// import { Container, Row, Col } from 'react-bootstrap';
+// import Text from '../Homepage/Text';
+// import { VscGithub } from "react-icons/vsc";
+// import { FaLinkedinIn } from "react-icons/fa";
+
+// function Home() {
+//   const googleDriveResumeLink = 'https://drive.google.com/file/d/1DDQyt8NPvm9krlI6Q1UrYOb3MAvFj3Mx/view';
+
+//   return (
+//     <div className='homebg-ground'>
+//       <Container>
+//         <Row>
+//           <Col md={7} className='backInUp'>
+//             <h2 className='headtext'>Hello</h2>
+//             <h2 className='nametext'>I'm Anand</h2>
+//             <span className="underline"></span>
+//             <Text className='backInUp' />
+//             <div className="social-media-buttons backInUp">
+//               <button
+//                 onClick={() => {
+//                   window.open("https://github.com/AnandYellapu");
+//                 }}
+//                 className='social-media-btn'
+//               >
+//                 <VscGithub className='icon github-color' />
+//               </button>
+
+//               <button
+//                 onClick={() => {
+//                   window.open("https://www.linkedin.com/in/yellapu-anandsai-38873b238/");
+//                 }}
+//                 className='social-media-btn'
+//               >
+//                 <FaLinkedinIn className='icon linkedin-color' />
+//               </button>
+//             </div>
+//             <button
+//               className="view-resume-button backInUp"
+//               onClick={() => {
+//                 window.open(googleDriveResumeLink);
+//               }}
+//             >
+//               View My Resume
+//             </button>
+//           </Col>
+//           <Col md={5} className='backInUp'>
+//             <div className="imagedeveloper backInUp"></div>
+//           </Col>
+//         </Row>
+//       </Container>
+//     </div>
+//   );
+// }
+
+// export default Home;
+
+
+
+
 import React from 'react';
 import '../../pages/style.css';
 import { Container, Row, Col } from 'react-bootstrap';
 import Text from '../Homepage/Text';
-import { VscGithub } from "react-icons/vsc";
-import { FaLinkedinIn } from "react-icons/fa";
+import { VscGithub } from 'react-icons/vsc';
+import { FaLinkedinIn } from 'react-icons/fa';
+import 'animate.css';
 
 function Home() {
-  const googleDriveResumeLink = 'https://drive.google.com/file/d/1DDQyt8NPvm9krlI6Q1UrYOb3MAvFj3Mx/view';
+  const googleDriveResumeLink =
+    'https://drive.google.com/file/d/1DDQyt8NPvm9krlI6Q1UrYOb3MAvFj3Mx/view';
 
   return (
-    <div className='homebg-ground'>
+    <div className='homebg-ground animate__animated animate__zoomIn'>
       <Container>
         <Row>
-          <Col md={7} className='backInUp'>
+          <Col md={7}>
             <h2 className='headtext'>Hello</h2>
             <h2 className='nametext'>I'm Anand</h2>
-            <span className="underline"></span>
-            <Text className='backInUp' />
-            <div className="social-media-buttons backInUp">
+            <span className='underline'></span>
+            <Text />
+            <div className='social-media-buttons'>
               <button
                 onClick={() => {
-                  window.open("https://github.com/AnandYellapu");
+                  window.open('https://github.com/AnandYellapu');
                 }}
                 className='social-media-btn'
               >
@@ -90,7 +153,9 @@ function Home() {
 
               <button
                 onClick={() => {
-                  window.open("https://www.linkedin.com/in/yellapu-anandsai-38873b238/");
+                  window.open(
+                    'https://www.linkedin.com/in/yellapu-anandsai-38873b238/'
+                  );
                 }}
                 className='social-media-btn'
               >
@@ -98,7 +163,7 @@ function Home() {
               </button>
             </div>
             <button
-              className="view-resume-button backInUp"
+              className='view-resume-button'
               onClick={() => {
                 window.open(googleDriveResumeLink);
               }}
@@ -106,8 +171,8 @@ function Home() {
               View My Resume
             </button>
           </Col>
-          <Col md={5} className='backInUp'>
-            <div className="imagedeveloper backInUp"></div>
+          <Col md={5}>
+            <div className='imagedeveloper'></div>
           </Col>
         </Row>
       </Container>
