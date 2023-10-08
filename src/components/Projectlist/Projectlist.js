fa28e6f87fd2f658909ba4f42ac99375b601446b
AnandYellapu <anandsaiii1200@gmail.com>
2023-10-08 12:48:56 +0530

    update

--- src/components/Projectlist/Projectlist.js
+++ src/components/Projectlist/Projectlist.js
@@ -3,9 +3,9 @@ import { motion, useTransform, useScroll } from 'framer-motion';
 import '../../pages/style.css';
 import ProjectCard from "./ProjectCard";
 import { Container, Row, Col } from 'react-bootstrap';
-import crypto from '../../Assets/crypto.jpg';
 import food from '../../Assets/food.png';
-import url from '../../Assets/url.png';
+import blog from '../../Assets/blog.png';
+import hotel from '../../Assets/hotel.png';
 
 function Projectlist() {
   const { scrollYProgress } = useScroll();
@@ -18,18 +18,7 @@ function Projectlist() {
       <Container fluid className="dash-section">
         <Container>
           <Row style={{ justifyContent: "center", paddingBottom: "40px", marginTop: "57px" }}>
-            <Col md={4} className="dash-card">
-              <motion.div style={{ opacity, x }}>
-                <ProjectCard
-                  imgPath={crypto}
-                  isBlog={false}
-                  title="CoinDex"
-                  description="A cryptocurrency exchange is a digital marketplace where users can buy, sell, and trade cryptocurrencies..."
-                  ghLink="https://myweb3.netlify.app/"
-                />
-              </motion.div>
-            </Col>
-
+           
             <Col md={4} className="dash-card">
               <motion.div style={{ opacity, x }}>
                 <ProjectCard
@@ -42,14 +31,27 @@ function Projectlist() {
               </motion.div>
             </Col>
 
+            
             <Col md={4} className="dash-card">
               <motion.div style={{ opacity, x }}>
                 <ProjectCard
-                  imgPath={url}
+                  imgPath={blog}
                   isBlog={false}
-                  title="URL SHORTENER"
-                  description="A URL shortener is a web service or application that converts long and cumbersome URLs..."
-                  ghLink="https://url-shorteneer.netlify.app"
+                  title="Mix"
+                  description="Mix blog project is a modern and versatile platform designed for seamless content creation..."
+                  ghLink="https://mixbloog.netlify.app"
+                />
+              </motion.div>
+            </Col>
+
+            <Col md={4} className="dash-card">
+              <motion.div style={{ opacity, x }}>
+                <ProjectCard
+                  imgPath={hotel}
+                  isBlog={false}
+                  title="Ebooking"
+                  description="Ebooking project is a comprehensive solution for managing hotel reservations...."
+                  ghLink="https://eboooking.netlify.app"
                 />
               </motion.div>
             </Col>
