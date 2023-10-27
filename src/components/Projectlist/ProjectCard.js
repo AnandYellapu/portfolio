7568038d57c5c8279fdbd4ff309a555ae44d4ef5
AnandYellapu <anandsaiii1200@gmail.com>
2023-10-27 06:53:46 +0530

    update

--- src/components/Projectlist/ProjectCard.js
+++ src/components/Projectlist/ProjectCard.js
@@ -1,3 +1,79 @@
+// import React from "react";
+// import Card from "react-bootstrap/Card";
+// import Button from "react-bootstrap/Button";
+// import { motion } from "framer-motion";
+// import {
+//   DiMongodb,
+//   DiReact,
+//   DiNodejsSmall,
+// } from "react-icons/di";
+// import { SiExpress, SiMui } from "react-icons/si";
+
+// function ProjectCard(props) {
+//   const renderTechStackIcons = (techStack) => {
+//     if (!techStack) {
+//       return null;
+//     }
+
+//     return techStack.split(",").map((tech, index) => (
+//       <span key={index} className="tech-icon">
+//         {getTechIcon(tech.trim().toLowerCase())}
+//       </span>
+//     ));
+//   };
+
+//   const getTechIcon = (tech) => {
+//     switch (tech) {
+//       case "mongodb":
+//         return <DiMongodb title="MongoDB" />;
+//       case "express":
+//         return <SiExpress title="Express" />;
+//       case "react":
+//         return <DiReact title="React" />;
+//       case "nodejs":
+//         return <DiNodejsSmall title="Node.js" />;
+//       case "material-ui":
+//         return <SiMui title="Material-Ui" />;
+//       default:
+//         return null;
+//     }
+//   };
+
+//   return (
+//     <motion.div
+//       initial={{ opacity: 0, y: 20 }}
+//       animate={{ opacity: 1, y: 0 }}
+//       transition={{ duration: 0.5 }}
+//       className="card-card"
+//     >
+//       <Card>
+//         <Card.Img variant="top" src={props.imgPath} alt="card-img" />
+//         <Card.Body className="d-flex flex-column">
+//           <Card.Title>{props.title}</Card.Title>
+//           <Card.Text>{props.description}</Card.Text>
+//           <div className="d-flex justify-content-center tech-stack">
+//             {renderTechStackIcons(props.techStack)}
+//           </div>
+//           <div className="d-flex justify-content-center">
+//             <Button
+//               className="btn p-200 m-9 h-5 w-100"
+//               variant="btn btn-outline-success"
+//               href={props.ghLink}
+//               target="_blank"
+//             >
+//               View
+//             </Button>
+//           </div>
+//         </Card.Body>
+//       </Card>
+//     </motion.div>
+//   );
+// }
+
+// export default ProjectCard;
+
+
+
 import React from "react";
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
@@ -8,6 +84,7 @@ import {
   DiNodejsSmall,
 } from "react-icons/di";
 import { SiExpress, SiMui } from "react-icons/si";
+import { FaGithub } from 'react-icons/fa';
 
 function ProjectCard(props) {
   const renderTechStackIcons = (techStack) => {
@@ -56,12 +133,20 @@ function ProjectCard(props) {
           </div>
           <div className="d-flex justify-content-center">
             <Button
-              className="btn p-200 m-9 h-5 w-100"
+              className="btn p-200 m-9 h-5 w-100 me-md-2"
               variant="btn btn-outline-success"
-              href={props.ghLink}
+              href={props.netlifyLink}
               target="_blank"
             >
               View
+            </Button>
+            <Button
+              className="btn p-200 m-9 h-5 w-100 ml-5" // Add margin to create a small gap
+              variant="btn btn-outline-dark"
+              href={props.ghLink}
+              target="_blank"
+            >
+              <FaGithub />
             </Button>
           </div>
         </Card.Body>
--- src/components/Projectlist/Projectlist.js
+++ src/components/Projectlist/Projectlist.js
@@ -1,5 +1,92 @@
+// import React from 'react';
+// import { motion, useTransform, useScroll } from 'framer-motion';
+// import '../../pages/style.css';
+// import ProjectCard from "./ProjectCard";
+// import { Container, Row, Col } from 'react-bootstrap';
+// import food from '../../Assets/food.png';
+// import blog from '../../Assets/blog.png';
+// import hotel from '../../Assets/hotel.png';
+// import ebook from '../../Assets/ebook.png';
+
+// function Projectlist() {
+//   const { scrollYProgress } = useScroll();
+
+//   const opacity = useTransform(scrollYProgress, [0, 0.5], [0, 1]);
+//   const x = useTransform(scrollYProgress, [0, 1], [-100, 0]);
+
+//   return (
+//     <div className="projectbackground">
+//       <Container fluid className="dash-section">
+//         <Container>
+//           <Row style={{ justifyContent: "center", paddingBottom: "40px", marginTop: "57px" }}>
+           
+//             <Col md={4} className="dash-card">
+//               <motion.div style={{ opacity, x }}>
+//                 <ProjectCard
+//                   imgPath={food}
+//                   isBlog={false}
+//                   title="Food Recipe"
+//                   description="Flavorful Eats is a delightful and user-friendly food recipe app designed to inspire both novice..."
+//                   techStack="MongoDB,Express,React,NodeJS"
+//                   netlifyLink="https://kitchen-recipe-management1.netlify.app/"
+//                 />
+//               </motion.div>
+//             </Col>
+
+            
+//             <Col md={4} className="dash-card">
+//               <motion.div style={{ opacity, x }}>
+//                 <ProjectCard
+//                   imgPath={blog}
+//                   isBlog={false}
+//                   title="MixBlog"
+//                   description="Mix blog project is a modern and versatile platform designed for seamless content creation..."
+//                   techStack="MongoDB,Express,React,NodeJS"
+//                   netlifyLink="https://mixbloog.netlify.app"
+//                 />
+//               </motion.div>
+//             </Col>
+
+//             <Col md={4} className="dash-card">
+//               <motion.div style={{ opacity, x }}>
+//                 <ProjectCard
+//                   imgPath={hotel}
+//                   isBlog={false}
+//                   title="Ebooking"
+//                   description="Ebooking project is a comprehensive solution for managing hotel reservations...."
+//                   techStack="MongoDB,Express,React,NodeJS"
+//                   netlifyLink="https://eboooking.netlify.app"
+//                 />
+//               </motion.div>
+//             </Col>
+
+//             <Col md={4} className="dash-card">
+//             <motion.div style={{ opacity, x }}>
+//               <ProjectCard
+//                 imgPath={ebook}
+//                 isBlog={false}
+//                 title="Ebook"
+//                 description="An Ebook Selling is an online platform designed to provide a convenient and user-friendly."
+//                 techStack="MongoDB,Express,React,NodeJS,Material-UI"
+//                 netlifyLink="https://eboook.netlify.app"
+//               />
+//             </motion.div>
+//           </Col>
+
+//           </Row>
+//         </Container>
+//       </Container>
+//     </div>
+//   );
+// }
+
+// export default Projectlist;
+
+
+
+
+
 import React from 'react';
-import { motion, useTransform, useScroll } from 'framer-motion';
 import '../../pages/style.css';
 import ProjectCard from "./ProjectCard";
 import { Container, Row, Col } from 'react-bootstrap';
@@ -9,11 +96,6 @@ import hotel from '../../Assets/hotel.png';
 import ebook from '../../Assets/ebook.png';
 
 function Projectlist() {
-  const { scrollYProgress } = useScroll();
-
-  const opacity = useTransform(scrollYProgress, [0, 0.5], [0, 1]);
-  const x = useTransform(scrollYProgress, [0, 1], [-100, 0]);
-
   return (
     <div className="projectbackground">
       <Container fluid className="dash-section">
@@ -21,57 +103,52 @@ function Projectlist() {
           <Row style={{ justifyContent: "center", paddingBottom: "40px", marginTop: "57px" }}>
            
             <Col md={4} className="dash-card">
-              <motion.div style={{ opacity, x }}>
-                <ProjectCard
-                  imgPath={food}
-                  isBlog={false}
-                  title="Food Recipe"
-                  description="Flavorful Eats is a delightful and user-friendly food recipe app designed to inspire both novice..."
-                  techStack="MongoDB,Express,React,NodeJS"
-                  ghLink="https://kitchen-recipe-management1.netlify.app/"
-                />
-              </motion.div>
-            </Col>
-
-            
-            <Col md={4} className="dash-card">
-              <motion.div style={{ opacity, x }}>
-                <ProjectCard
-                  imgPath={blog}
-                  isBlog={false}
-                  title="MixBlog"
-                  description="Mix blog project is a modern and versatile platform designed for seamless content creation..."
-                  techStack="MongoDB,Express,React,NodeJS"
-                  ghLink="https://mixbloog.netlify.app"
-                />
-              </motion.div>
+              <ProjectCard
+                imgPath={food}
+                isBlog={false}
+                title="Food Recipe"
+                description="Flavorful Eats is a delightful and user-friendly food recipe app designed to inspire both novice..."
+                techStack="MongoDB, Express, React, NodeJS"
+                netlifyLink="https://kitchen-recipe-management1.netlify.app/"
+                ghLink="https://github.com/AnandYellapu/kitchen-recipe-frontend"
+              />
             </Col>
 
             <Col md={4} className="dash-card">
-              <motion.div style={{ opacity, x }}>
-                <ProjectCard
-                  imgPath={hotel}
-                  isBlog={false}
-                  title="Ebooking"
-                  description="Ebooking project is a comprehensive solution for managing hotel reservations...."
-                  techStack="MongoDB,Express,React,NodeJS"
-                  ghLink="https://eboooking.netlify.app"
-                />
-              </motion.div>
+              <ProjectCard
+                imgPath={blog}
+                isBlog={false}
+                title="MixBlog"
+                description="Mix blog project is a modern and versatile platform designed for seamless content creation..."
+                techStack="MongoDB, Express, React, NodeJS"
+                netlifyLink="https://mixbloog.netlify.app/"
+                ghLink="https://github.com/AnandYellapu/blog-fe"
+              />
             </Col>
 
             <Col md={4} className="dash-card">
-            <motion.div style={{ opacity, x }}>
+              <ProjectCard
+                imgPath={hotel}
+                isBlog={false}
+                title="Ebooking"
+                description="Ebooking project is a comprehensive solution for managing hotel reservations...."
+                techStack="MongoDB, Express, React, NodeJS"
+                netlifyLink="https://eboooking.netlify.app/"
+                ghLink="https://github.com/AnandYellapu/Ebooking-fe"
+              />
+            </Col>
+
+            <Col md={4} className="dash-card">
               <ProjectCard
                 imgPath={ebook}
                 isBlog={false}
                 title="Ebook"
                 description="An Ebook Selling is an online platform designed to provide a convenient and user-friendly."
-                techStack="MongoDB,Express,React,NodeJS,Material-UI"
-                ghLink="https://eboook.netlify.app"
+                techStack="MongoDB, Express, React, NodeJS, Material-UI"
+                netlifyLink="https://eboook.netlify.app/"
+                ghLink="https://github.com/AnandYellapu/Ebook-fe"
               />
-            </motion.div>
-          </Col>
+            </Col>
 
           </Row>
         </Container>
