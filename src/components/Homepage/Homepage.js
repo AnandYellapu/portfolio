7f9d1c5cfef4571dc476f0e25f1a3a75956a019e
AnandYellapu <anandsaiii1200@gmail.com>
2025-05-23 12:15:58 +0530

    update

--- src/components/Homepage/Homepage.js
+++ src/components/Homepage/Homepage.js
@@ -193,6 +193,81 @@
 
 
 
+// import React from 'react';
+// import '../../pages/style.css';
+// import { Container, Row, Col } from 'react-bootstrap';
+// import Text from '../Homepage/Text';
+// import { VscGithub } from 'react-icons/vsc';
+// import { FaLinkedinIn } from 'react-icons/fa';
+// import 'animate.css';
+
+// function Home() {
+//   const googleDriveResumeLink = 'https://drive.google.com/file/d/1DDQyt8NPvm9krlI6Q1UrYOb3MAvFj3Mx/view';  //eslint-disable-line
+
+//   return (
+//     <div className='homebg-ground animate__animated animate__zoomIn'>
+//       <Container>
+//         <Row>
+//           <Col md={7}>
+//             <h2 className='headtext'>Hello</h2>
+//             <h2 className='nametext'>I'm Anand</h2>
+//             <span className='underline'></span>
+//             <Text />
+//             <div className='social-media-buttons'>
+//               <button
+//                 onClick={() => {
+//                   window.open('https://github.com/AnandYellapu', '_blank', 'noopener,noreferrer');
+//                 }}
+//                 className='social-media-btn'
+//               >
+//                 <VscGithub className='icon github-color' />
+//               </button>
+
+//               <button
+//                 onClick={() => {
+//                   window.open(
+//                     'https://www.linkedin.com/in/yellapu-anandsai-38873b238/',
+//                     '_blank',
+//                     'noopener,noreferrer'
+//                   );
+//                 }}
+//                 className='social-media-btn'
+//               >
+//                 <FaLinkedinIn className='icon linkedin-color' />
+//               </button>
+//             </div>
+
+//             {/* Resume Button - Uncomment if needed */}
+//             {/* 
+//             <button
+//               className='view-resume-button'
+//               onClick={() => {
+//                 window.open(googleDriveResumeLink, '_blank', 'noopener,noreferrer');
+//               }}
+//             >
+//               View My Resume
+//             </button>
+//             */}
+//           </Col>
+
+//           <Col md={5}>
+//             <div className='imagedeveloper'></div>
+//           </Col>
+//         </Row>
+//       </Container>
+//     </div>
+//   );
+// }
+
+// export default Home;
+
+
+
+
+
+
+
+
 import React from 'react';
 import '../../pages/style.css';
 import { Container, Row, Col } from 'react-bootstrap';
@@ -202,8 +277,6 @@ import { FaLinkedinIn } from 'react-icons/fa';
 import 'animate.css';
 
 function Home() {
-  const googleDriveResumeLink = 'https://drive.google.com/file/d/1DDQyt8NPvm9krlI6Q1UrYOb3MAvFj3Mx/view';  //eslint-disable-line
-
   return (
     <div className='homebg-ground animate__animated animate__zoomIn'>
       <Container>
@@ -236,18 +309,6 @@ function Home() {
                 <FaLinkedinIn className='icon linkedin-color' />
               </button>
             </div>
-
-            {/* Resume Button - Uncomment if needed */}
-            {/* 
-            <button
-              className='view-resume-button'
-              onClick={() => {
-                window.open(googleDriveResumeLink, '_blank', 'noopener,noreferrer');
-              }}
-            >
-              View My Resume
-            </button>
-            */}
           </Col>
 
           <Col md={5}>
--- src/components/Homepage/Text.js
+++ src/components/Homepage/Text.js
@@ -1,16 +1,51 @@
+// import React, { useRef } from 'react';
+// import '../../pages/style.css';
+// import Typewriter from "typewriter-effect";
+
+// function Text() {
+//   const typewriterRef = useRef(null);
+
+//   const handleClick = () => {
+//     typewriterRef.current.stop();
+//     setTimeout(() => {
+//       typewriterRef.current.start();
+//     }, 2000);
+//   };
+
+//   return (
+//     <div className="typewriter-container">
+//       <Typewriter
+//         options={{
+//           strings: [
+//             "Web Developer", "Designer"
+//           ],
+//           autoStart: true,
+//           loop: true,
+//           deleteSpeed: 30,
+//         }}
+//         onInit={(typewriter) => {
+//           typewriterRef.current = typewriter;
+//         }}
+//         className="typewriter-text" // Add class name to the typewriter text
+//       />
+//       <button onClick={handleClick} className="typewriter-btn">Typing</button>
+//     </div>
+//   )
+// }
+
+// export default Text;
+
+
+
+
+
+
 import React, { useRef } from 'react';
 import '../../pages/style.css';
 import Typewriter from "typewriter-effect";
 
 function Text() {
   const typewriterRef = useRef(null);
-
-  const handleClick = () => {
-    typewriterRef.current.stop();
-    setTimeout(() => {
-      typewriterRef.current.start();
-    }, 2000);
-  };
 
   return (
     <div className="typewriter-container">
@@ -26,11 +61,11 @@ function Text() {
         onInit={(typewriter) => {
           typewriterRef.current = typewriter;
         }}
-        className="typewriter-text" // Add class name to the typewriter text
+        className="typewriter-text"
       />
-      <button onClick={handleClick} className="typewriter-btn">Typing</button>
     </div>
   )
 }
 
 export default Text;
+
